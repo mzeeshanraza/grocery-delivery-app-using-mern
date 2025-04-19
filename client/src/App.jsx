40ca9c3c2271f96da903fa8051835bc16063969b
mzeeshanraza <mzeeshanraza77@gmail.com>
2025-04-19 16:12:33 +0500

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
+  const isSellerPath = useLocation().pathname.includes("seller");
+
   return (
     <>
-      <Navbar />
+      {isSellerPath ? null : <Navbar />}
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"} `}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </>
   );
 }
